08050e5b95833c063094bebf54d3b8b8f100cfd6
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-15 21:08:34 +0300

    add dz03

---
+++ dz03/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+
+void FillMas(int[]collection)
+{
+    int length=collection.Length;
+    int index=0;
+    while(index< length)
+    {
+        collection[index]=new Random().Next(100,1000);
+        index++;
+    }
+}
+
+void PrintMas(int[]col)
+{
+    int dlinna=col.Length;
+    int i=0;
+    while(i<dlinna)
+    {
+        Console.WriteLine(col[i]);
+        i++;
+    }
+}
+
+int[] massiv=new int[10];
+
+FillMas(massiv);
+PrintMas(massiv);
+
+// int n1=0;
+// int n2=0;
+int ind=0;
+int n1=0;
+int n2=0;
+
+
+while(ind<massiv.Length)
+{
+    if (massiv[ind]%2==0)
+    {  
+        n1++;
+    }
+    else
+    {
+        n2++;
+    }
+    ind++;   
+}
+Console.WriteLine("Вывод количества четных чисел: " + n1);
+Console.WriteLine("Вывод количества нечетных чисел: " + n2);
