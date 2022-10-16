75166d664076fc7c358fa593620f09e345018126
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-16 13:47:22 +0300

    add dz07

---
+++ dz07/Program.cs
@@ -0,0 +1,49 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+
+void FillMas(double[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 15);
+        index++;
+    }
+}
+
+void PrintMas(double[] col)
+{
+    int dlinna = col.Length;
+    int i = 0;
+    while (i < dlinna)
+    {
+        Console.Write(col[i] + "    ");
+        i++;
+    }
+}
+
+double[] massiv = new double[12];
+
+FillMas(massiv);
+PrintMas(massiv);
+Console.WriteLine();
+
+int ind = 0;
+double min = massiv[0];
+double max = massiv[0];
+
+while (ind < massiv.Length)
+{
+    if (min > massiv[ind]) min = massiv[ind];
+    if (max < massiv[ind]) max = massiv[ind];
+    ind++;
+}
+Console.WriteLine("min число: " + min);
+Console.WriteLine("max число: " + max);
+Console.WriteLine("Разница: " + (max - min));
+
+
+
+
+
