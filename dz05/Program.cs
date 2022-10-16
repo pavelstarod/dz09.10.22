0bf1a46ab1b0a1b15b838c303c463453a6bd5789
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-16 12:13:03 +0300

    add dz05

--- dz05/Program.cs
+++ dz05/Program.cs
@@ -1,41 +1,41 @@
 ﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
 
 
-void FillMas(int[]collection)
+void FillMas(int[] collection)
 {
-    int lenght=collection.Length;
-    int index=0;
-    while(index<lenght)
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
     {
-        collection[index]=new Random().Next(1,10);
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
 
-void PrintMas(int[]col)
+void PrintMas(int[] col)
 {
-    int dlinna=col.Length;
-    int i=0;
-    while(i<dlinna)
+    int dlinna = col.Length;
+    int i = 0;
+    while (i < dlinna)
     {
         Console.Write(col[i] + "  ");
         i++;
     }
 }
 
-int[]massiv=new int[10];
+int[] massiv = new int[10];
 
 FillMas(massiv);
 PrintMas(massiv);
 Console.WriteLine();
 
-int ind=0;
-int result=0;
-while(ind<massiv.Length)
+int ind = 0;
+int result = 0;
+while (ind < massiv.Length)
 {
-    if(massiv[ind]%2==1)
+    if (massiv[ind] % 2 == 1)
     {
-        result=result+massiv[ind];
+        result = result + massiv[ind];
     }
     ind++;
 }
